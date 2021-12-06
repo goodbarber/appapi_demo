8085630a845f22d8f5c689a4e3882c3f7ac29860
Christophe Lucchini <christophelucchini@macbook-pro-de-christophe.home>
2021-12-06 10:55:06 +0100

    update gbjstk

--- GBJSTK.js
+++ GBJSTK.js
@@ -360,18 +360,6 @@ function gbRequest ( resourceUrl, tag, cache, requestMethod, postParams )
 
 /************* [GB Plugin API] Other Methods *************/
 
-/* Function : gbAuthenticate
-*  Ask the user to authenticate on a social network.
-*  @param services The services to use for the authentication | values : [all(default)|facebook|twitter]
-*  @param skip Give the user the possibility to skip the authentication process | values : [YES(default)|NO]
-*/
-function gbAuthenticate ( services, skip )
-{
-	services = services || "all";
-	skip = skip || "YES";
-	gbGetRequest ( "goodbarber://authenticate", { "services":services, "skip":skip } );
-}
-
 /* Function : gbShare
 *  Ask the user to share a content on a social network.
 *  @param shareText The text to share
@@ -515,7 +503,11 @@ function gbAlert( title, message )
 */
 function gbPrint()
 {
-	gbGetRequest ( "goodbarber://print" );
+	if (!gbAngularMode) {
+        gbGetRequest ( "goodbarber://print" );
+    } else {
+        window.print();
+    }
 }
 
 /************* Website *************/
